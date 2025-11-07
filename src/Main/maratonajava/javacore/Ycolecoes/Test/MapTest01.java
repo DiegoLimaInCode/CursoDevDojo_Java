241090b2e5b5af4b887eaeaa33be90961c34b3b5
Diego <diegolimabarenco@gmail.com>
2025-11-07 15:47:27 -0300

    aula178

---
+++ src/Main/maratonajava/javacore/Ycolecoes/Test/MapTest01.java
@@ -0,0 +1,26 @@
+package Main.maratonajava.javacore.Ycolecoes.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MapTest01 {
+    static void main() {
+        Map<String, String> map = new HashMap<>();
+        map.put("teklado", "teclado");
+        map.put("mouze", "mouse");
+        map.put("vc", "você");
+        System.out.println(map);
+        System.out.println("------------");
+        for (String key : map.keySet()) {
+            System.out.println(key + " : " + map.get(key));
+        }
+        System.out.println("----------");
+        for (String value : map.values()) {
+            System.out.println(value);
+        }
+        System.out.println("-----------");
+        for (Map.Entry<String, String> entry : map.entrySet()) {
+            System.out.println(entry.getKey() + " - " + entry.getValue());
+        }
+    }
+}
