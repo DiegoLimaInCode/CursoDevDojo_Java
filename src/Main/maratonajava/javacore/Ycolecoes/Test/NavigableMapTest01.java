f476ffa5ce230ae5954cbb7eb9df6496a890dcad
Diego <diegolimabarenco@gmail.com>
2025-11-07 17:46:47 -0300

    aula 181

---
+++ src/Main/maratonajava/javacore/Ycolecoes/Test/NavigableMapTest01.java
@@ -0,0 +1,23 @@
+package Main.maratonajava.javacore.Ycolecoes.Test;
+
+import java.util.Map;
+import java.util.NavigableMap;
+import java.util.TreeMap;
+
+public class NavigableMapTest01 {
+    static void main() {
+        NavigableMap<String, String> map = new TreeMap<>();
+        map.put("A","Letra A");
+        map.put("B","Letra B");
+        map.put("C","Letra C");
+        map.put("D","Letra D");
+        map.put("E","Letra E");
+
+        for (Map.Entry<String, String> entry : map.entrySet()) {
+            System.out.println(entry.getKey()+"-"+entry.getValue());
+        }
+
+        System.out.println(map.headMap("C",true));
+        System.out.println(map.ceilingEntry());
+    }
+}
