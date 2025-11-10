d865e251fd2dabfe17eee08ae142bede31eedeba
Diego <diegolimabarenco@gmail.com>
2025-11-10 18:04:02 -0300

    aula 185

--- src/Main/maratonajava/javacore/Zgenerics/Test/WildCardTest02.java
+++ src/Main/maratonajava/javacore/Zgenerics/Test/WildCardTest02.java
@@ -8,13 +8,22 @@ public class WildCardTest02 {
     static void main() {
         List<Cachorro> cachorros = List.of(new Cachorro(), new Cachorro());
         List<Gato> gatos = List.of(new Gato(), new Gato());
-
-
+        printConsulta(cachorros);
+        printConsulta(gatos);
+        List<Animal> animals2 = new ArrayList<>();
+        printConsultaAnimal(animals2);
     }
 
-    private static void printConsulta(List<Animal> animals) {
+    private static void printConsulta(List<? extends Animal> animals) {
         for (Animal animal : animals) {
             animal.consulta();
         }
+    }
+
+    private static void printConsultaAnimal(List<? super Animal> animals) {
+        Animal animal= new Cachorro();
+        Animal animal2= new Gato();
+        animals.add(new Cachorro());
+        animals.add(new Gato());
     }
 }
