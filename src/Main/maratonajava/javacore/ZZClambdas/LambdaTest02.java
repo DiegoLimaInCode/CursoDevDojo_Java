85add0b60c66dd3e49b048ca821ecceefbaa9fde
Diego <diegolimabarenco@gmail.com>
2025-12-28 21:35:41 -0300

    aula 197

---
+++ src/Main/maratonajava/javacore/ZZClambdas/LambdaTest02.java
@@ -0,0 +1,24 @@
+package Main.maratonajava.javacore.ZZClambdas;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Function;
+
+public class LambdaTest02 {
+    static void main() {
+        List<String> strings = List.of("natsu", "allucard");
+        List<Integer> integers = map(strings, s -> s.length());
+        List<String> map = map(strings, s -> s.toUpperCase());
+        System.out.println(integers);
+        System.out.println(map);
+    }
+
+    private static <T, R> List<R> map(List<T> list, Function<T, R> function) {
+        List<R> result = new ArrayList<>();
+        for (T e : list) {
+            R r = function.apply(e);
+            result.add(r);
+        }
+        return result;
+    }
+}
